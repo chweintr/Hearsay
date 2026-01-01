20724b9f3b4a2894598dda4e559de05a80eff087
OkDonut <calebweintraub@gmail.com>
2026-01-01 11:34:46 -0500

    Add audio system with ambient/music sliders, update background video path

---
+++ assets/videos/Background_1_files/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_kAGbWkK4abpnYLbpray83QyxeQ9laeDcxnwutgnz78y'] = {
+    config: {"token": "phc_kAGbWkK4abpnYLbpray83QyxeQ9laeDcxnwutgnz78y", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": true, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": false, "web_vitals_allowed_metrics": null}, "autocapture_opt_out": true, "autocaptureExceptions": true, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": true, "suppressionRules": []}, "sessionRecording": {"endpoint": "/s/", "consoleLogRecordingEnabled": true, "recorderVersion": "v2", "sampleRate": null, "minimumDurationMilliseconds": null, "linkedFlag": null, "networkPayloadCapture": null, "masking": null, "urlTriggers": [], "urlBlocklist": [], "eventTriggers": [], "triggerMatchType": null, "scriptConfig": {"script": "posthog-recorder"}}, "heatmaps": false, "surveys": false, "productTours": false, "defaultIdentifiedOnly": true},
+    siteApps: []
+  }
+})();

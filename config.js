708fb86751d5a8905909b24c2dc33795127af97e
OkDonut <calebweintraub@gmail.com>
2026-01-06 01:05:21 -0500

    Add Priya walkup video to config

--- config.js
+++ config.js
@@ -149,13 +149,13 @@ export const characters = {
     priya: {
         name: 'Priya',
         role: 'Bartender',
-        agentId: 'xxx',
-        faceId: 'xxx',
-        idleToActive: [],
-        activeToIdle: [],
-        knockSound: null,
-        previewVideo: null,
-        status: 'coming_soon'
+        agentId: 'xxx',  // TODO: Add when Simli agent created
+        faceId: 'xxx',   // TODO: Add when Simli face created
+        idleToActive: ['assets/videos/Priya_Walkuup.mp4'],
+        activeToIdle: ['assets/videos/Priya_Walkuup.mp4'],
+        knockSound: 'assets/sounds/door_knocks/knock_hotel_1.wav',
+        previewVideo: 'assets/videos/Priya_Walkuup.mp4',
+        status: 'coming_soon'  // Change to 'ready' when agent ID added
     },
     
     lenny: {
